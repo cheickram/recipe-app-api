81af9770c383df548adc6c3fc71a3c05719f857f
raimane <cheickram@gmail.com>
2023-06-14 23:52:58 +0000

    Added postgres connection and installed the dependencies and finally removed sqlite file auto generated by the django project

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+'''
+Calculator functions
+'''
+
+def add(x, y):
+    '''Ádd x and y and return result.'''
+    return x + y
+
+
+def subtract(x, y):
+    '''Subtract x and y and return result.'''
+    return x - y
